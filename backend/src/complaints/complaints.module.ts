0c9dc29750091cb300d35a656f7a9013534c4995
jersputra4 <jersputra4@gmail.com>
2026-09-20 17:10:10 +0700

    Add files via upload

--- backend/src/complaints/complaints.module.ts
+++ backend/src/complaints/complaints.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { DocumentsModule } from '../documents/documents.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { ReportsModule } from '../reports/reports.module';
 import { ComplaintsController } from './complaints.controller';
 import { ComplaintsService } from './complaints.service';
+import { KomdigiForwardService } from './komdigi-forward.service';
 
 @Module({
-  imports: [ReportsModule, NotificationsModule],
+  imports: [ReportsModule, NotificationsModule, DocumentsModule],
   controllers: [ComplaintsController],
-  providers: [ComplaintsService],
-  exports: [ComplaintsService],
+  providers: [ComplaintsService, KomdigiForwardService],
+  exports: [ComplaintsService, KomdigiForwardService],
 })
 export class ComplaintsModule {}
